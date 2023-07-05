3b80172d938f1aca95c74ba132454a6ef4650065
MihrijeI <137301870+MihrijeI@users.noreply.github.com>
2023-07-05 15:25:10 +0200

    Design (update)

--- resources/views/admin/body/footer.blade.php
+++ resources/views/admin/body/footer.blade.php
@@ -2,11 +2,12 @@
                     <div class="container-fluid">
                         <div class="row">
                             <div class="col-sm-6">
-                                 EasyLearning © Upcube.
+                                 CopyRight © 2023.
                             </div>
                             <div class="col-sm-6">
                                 <div class="text-sm-end d-none d-sm-block">
-                                    Crafted with <i class="mdi mdi-heart text-danger"></i> by EasyLearning
+                                    
+                                Made with &#10084;&#65039; by progtelx
                                 </div>
                             </div>
                         </div>
